b000fe62eadc6e79c192126527eb97d80e5a5348
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2017-08-08 19:40:16 +0800

    refactored code

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,37 @@
 module.exports.homeList = function(req, res){
-	res.render('locations-list', {title: 'Home'});
+	res.render('locations-list', {
+		title: 'Loc8r - Find a place to work with Wi-Fi',
+		pageHeader: {
+			title: 'Loc8r',
+			strapline: 'Find places to work with Wi-Fi near you!'
+		},
+		sidebar: "Looking for Wi-Fi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+		locations: [{
+			name: 'Startcups',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 3,
+			facilities: ['Hot Drinks', 'Food', 'Wi-Fi'],
+			distance: '100m'
+		}, {
+			name: 'Coffee Hero',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 4,
+			facilities: ['Hot Drinks', 'Food', 'Wi-Fi'],
+			distance: '200m'
+		}, {
+			name: 'Burger Queen',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 1,
+			facilities: ['Food', 'Wi-Fi'],
+			distance: '150m'
+		}, {
+			name: 'Kopitiam Kita',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 5,
+			facilities: ['Hot Drinks', 'Food'],
+			distance: '150m'
+		}]
+	});
 }
 
 module.exports.locationInfo = function(req, res){
